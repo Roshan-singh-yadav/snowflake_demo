c70cb49841837237f0c269aafe11affdd66b3f0d
Roshan-singh-yadav <167501032+Roshan-singh-yadav@users.noreply.github.com>
2024-06-07 10:12:25 +0000

    allow cors

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
+const cors = require('cors')
 const express = require('express');
 const bodyParser = require("body-parser");
 
 // for establishing connection
 const connection = require("./libraries/connect")
 const homeRoute = require("./routes/index")
-
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors())
 
 app.use("/", homeRoute);
 
