74b13c74ef4becc6865f0f15876a8c24d93523f0
Roshan-singh-yadav <167501032+Roshan-singh-yadav@users.noreply.github.com>
2024-06-07 10:36:34 +0000

    asdf

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const cors = require('cors')
 const express = require('express');
+const bodyParser = require('body-parser')
 
 // for establishing connection
 const homeRoute = require("./routes/index")
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(express.json());
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 
 app.use(cors())
 
