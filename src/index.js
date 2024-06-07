b3eb156ff55c5d296b06c71f768411e0df4a2914
Roshan-singh-yadav <167501032+Roshan-singh-yadav@users.noreply.github.com>
2024-06-07 10:27:12 +0000

    fix

--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ const homeRoute = require("./routes/index")
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(bodyParser.urlencoded());
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
 
 app.use(cors())
 
